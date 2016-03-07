7ea600a7c4b2782e45ca2f7ca2dfcfc4baf6bdb1
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-03-07 00:04:57 +0000

    Updated utils.js file.

--- arbritrage/utils.js
+++ arbritrage/utils.js
@@ -1,6 +1,33 @@
-// http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html
+// Utilities common to our econ Javascript applets
+
+//////////////////////////////////// Event handling
+
+function getMousePos(canvas, evt) {
+    var r = canvas.getBoundingClientRect();
+    return {x: evt.clientX - r.left, y: evt.clientY - r.top};
+}
+
+//////////////////////////////////// Geometry
+
+// distance between a point and a line segment
+// from: http://stackoverflow.com/a/1501725/2680707
+
+function dist2(vx, vy, wx, wy) {
+    return (vx-wx)*(vx-wx) + (vy-wy)*(vy-wy);
+}
+function distToSegmentSq(px, py, vx, vy, wx, wy) {
+    var l2 = dist2(vx, vy, wx, wy);
+    if (l2 == 0) return dist2(px, py, vx, vy);
+    var t = ((px - vx) * (wx - vx) + (py - vy) * (wy - vy)) / l2;
+    if (t < 0) return dist2(px, py, vx, vy);
+    if (t > 1) return dist2(px, py, wx, wy);
+    return dist2(px, py, vx + t*(wx-vx), vy + t*(wy-vy));
+}
+
+//////////////////////////////////// Random generators
 
 function gauss_rand(mean, sd) {
+    // http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html
     if (mean == undefined)
         mean = 0.0;
     if (sd == undefined)
@@ -19,20 +46,5 @@ function gauss_rand(mean, sd) {
     //Y = Math.sqrt(-2 * Math.log(S) / S) * V2;
     //Y = mean + sd * Y ;
     return X;
-}
-
-// distance between a point and a line segment
-// from: http://stackoverflow.com/a/1501725/2680707
-
-function dist2(vx, vy, wx, wy) {
-    return (vx-wx)*(vx-wx) + (vy-wy)*(vy-wy);
-}
-function distToSegmentSq(px, py, vx, vy, wx, wy) {
-    var l2 = dist2(vx, vy, wx, wy);
-    if (l2 == 0) return dist2(px, py, vx, vy);
-    var t = ((px - vx) * (wx - vx) + (py - vy) * (wy - vy)) / l2;
-    if (t < 0) return dist2(px, py, vx, vy);
-    if (t > 1) return dist2(px, py, wx, wy);
-    return dist2(px, py, vx + t*(wx-vx), vy + t*(wy-vy));
 }
 
