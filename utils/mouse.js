3e7b1d0b9700e43e0aae8bdc59ae2ffbac0393a6
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-03-25 20:01:11 +0000

    Moved shared things to a utils folder.

--- utils/mouse.js
+++ utils/mouse.js
@@ -24,27 +24,3 @@ function distToSegmentSq(px, py, vx, vy, wx, wy) {
     return dist2(px, py, vx + t*(wx-vx), vy + t*(wy-vy));
 }
 
-//////////////////////////////////// Random generators
-
-function gauss_rand(mean, sd) {
-    // http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html
-    if (mean == undefined)
-        mean = 0.0;
-    if (sd == undefined)
-        sd = 1.0;
-    var V1, V2, S;
-    do {
-        var U1 = Math.random();
-        var U2 = Math.random();
-        V1 = 2 * U1 - 1;
-        V2 = 2 * U2 - 1;
-        S = V1 * V1 + V2 * V2;
-    } while (S > 1);
-
-    X = Math.sqrt(-2 * Math.log(S) / S) * V1;
-    X = mean + sd * X;
-    //Y = Math.sqrt(-2 * Math.log(S) / S) * V2;
-    //Y = mean + sd * Y ;
-    return X;
-}
-
