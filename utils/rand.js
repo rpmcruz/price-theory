c411efb54d05af8ec9033f1bf203872ba8115f26
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-04-13 21:57:24 +0100

    Moved fisher yates shuffle to library.

--- utils/rand.js
+++ utils/rand.js
@@ -1,6 +1,5 @@
-// http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html
-
-function gauss_rand(mean, sd) {
+function randn(mean, sd) {
+    // http://blog.yjl.im/2010/09/simulating-normal-random-variable-using.html
     if (mean == undefined)
         mean = 0.0;
     if (sd == undefined)
@@ -16,7 +15,24 @@ function gauss_rand(mean, sd) {
 
     X = Math.sqrt(-2 * Math.log(S) / S) * V1;
     X = mean + sd * X;
-    //Y = Math.sqrt(-2 * Math.log(S) / S) * V2;
-    //Y = mean + sd * Y ;
+    Y = Math.sqrt(-2 * Math.log(S) / S) * V2;
+    Y = mean + sd * Y ;
+    return [X, Y];
+}
+
+// random vector
+
+function fisher_yates(n) {
+    // see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    X = new Array(n);
+    for(var i = 0; i < n; i++)
+        X[i] = i;
+    for(var i = 0; i <= n-2; i++) {
+        var j = Math.floor(Math.random()*(n-i));
+        // exchange a[i] and a[i+j]
+        var t = X[i];
+        X[i] = X[i+j];
+        X[i+j] = t;
+    }
     return X;
 }
