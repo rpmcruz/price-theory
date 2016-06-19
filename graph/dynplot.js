27eb58222aa48b422b0462c0a0d407e0c59fc995
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-06-19 13:50:50 +0100

    Re-worked dyn plot framework.

--- graph/dynplot.js
+++ graph/dynplot.js
@@ -1,8 +1,10 @@
+//** DynPlot
+
 const MARGIN = {top:10, left:75, bottom:60, right:15};
 const WIDTH = 600 - MARGIN.left - MARGIN.right;
 const HEIGHT = 350 - MARGIN.top - MARGIN.bottom;
 
-var Dynplot = function(container, xlim, ylim)
+function DynPlot(container, xlim, ylim)
 {
     var plot = this;
     this.xscale = d3.scale.linear().domain([0, xlim]).range([0, WIDTH]);
@@ -37,7 +39,7 @@ var Dynplot = function(container, xlim, ylim)
         });
 }
 
-Dynplot.prototype.xaxis = function(title)
+DynPlot.prototype.xaxis = function(title)
 {
     var xaxis = d3.svg.axis().scale(this.xscale).orient('bottom');
     this.graph.append('g')
@@ -52,9 +54,10 @@ Dynplot.prototype.xaxis = function(title)
             .attr('transform', 'translate(' + (WIDTH/2) + ',' +
                 (HEIGHT+MARGIN.bottom-5) + ')')
             .text(title);
+    return this;
 };
 
-Dynplot.prototype.yaxis = function(title)
+DynPlot.prototype.yaxis = function(title)
 {
     var yaxis = d3.svg.axis().scale(this.yscale).ticks(4).orient('left');
     this.graph.append('g')
@@ -68,17 +71,21 @@ Dynplot.prototype.yaxis = function(title)
             .attr('text-anchor', 'middle')
             .attr('transform', 'translate(0,' + (HEIGHT/2) + ')rotate(-90)')
             .text(title);
+    return this;
 };
 
-Dynplot.prototype.addLine = function(klass, data, interactive) {
-    var plot = this;
-    var path = this.graph.selectAll('path.' + klass)
+//** DynLine
+
+function DynLine(plot, klass, data, interactive) {
+    var line = this;
+    this.plot = plot;
+    this.listeners = [];
+    this.path = plot.graph.selectAll('path.' + klass)
         .data([data]).enter().append('path')
         .attr('class', klass + ' line')
         .attr('d', function(d) {return plot.line(d);});
 
     if(interactive) {
-        var plot = this;
         var dragListener = d3.behavior.drag()
             .on('dragstart', function() {
                 d3.event.sourceEvent.stopPropagation();
@@ -86,21 +93,24 @@ Dynplot.prototype.addLine = function(klass, data, interactive) {
             })
             .on('drag', function(d, i) {
                 var x = d3.event.x, y = d3.event.y;
-                x = Math.min(
-                    Math.max(x, plot.xscale(data[i][0])),
+                x = Math.min(Math.max(x, plot.xscale(data[i][0])),
                     plot.xscale(data[i+2][0]));
+                y = Math.min(Math.max(y, plot.yscale.range()[1]),
+                    plot.yscale.range()[0]);
                 d[0] = plot.xscale.invert(x);
                 d[1] = plot.yscale.invert(y);
                 d3.select(this).attr('cx', x).attr('cy', y);
 
-                var path = plot.graph.selectAll('path.' + klass)
+                line.path
                     .data([data])
                     .attr('d', function(d) {return plot.line(d);});
+                for(var i = 0; i < line.listeners.length; i++)
+                    line.listeners[i].updated(line);
             })
             .on('dragend', function() {
                 d3.select(this).classed('dragging', false);
             });
-        var controls = this.graph.selectAll('circle.control.' + klass)
+        plot.graph.selectAll('circle.control.' + klass)
             .data(data)
             .enter()
             .append('circle')
@@ -113,36 +123,52 @@ Dynplot.prototype.addLine = function(klass, data, interactive) {
             .attr('cy', function(d) {return plot.yscale(d[1]);})
             .call(dragListener);
     }
-    return path;
 }
+
+//** DynPointIntersection
 
 function getIntersection(c1, c2) {
     // pretty fast code to find the intersection between two SVG paths
-    c1 = c1.node();
-    c2 = c2.node();
     var p1 = c1.getPointAtLength(0), p2 = c2.getPointAtLength(0);
     var oldp1 = p1, oldp2 = p2;
     var i = 0, j = 0;
-    for(var x = 0; x < WIDTH; x += 10) {
+    for(var x = 0; x < WIDTH; x += 2) {
         while(p1.x < x)
             p1 = c1.getPointAtLength(++i);
         while(p2.x < x)
             p2 = c2.getPointAtLength(++j);
         if(Math.sign(p1.y-p2.y) != Math.sign(oldp1.y-oldp2.y))
-            return {x:(p1.x+p2.x+oldp1.x+oldp2.x)/4,
-                    y:(p1.y+p2.y+oldp1.y+oldp2.y)/4};
+            return [[(p1.x+p2.x+oldp1.x+oldp2.x)/4,
+                     (p1.y+p2.y+oldp1.y+oldp2.y)/4]];
         oldp1 = p1;
         oldp2 = p2;
     }
-    return null;
+    return [];
 }
 
-Dynplot.prototype.addIntersection = function(c1, c2) {
-    var pt = getIntersection(c1, c2);
-    if(pt != null)
-        this.graph.append('circle')
-            .attr('class', 'intersection')
-            .attr('cx', pt.x)
-            .attr('cy', pt.y)
-            .attr('r', 6);
+function DynPointIntersection(line1, line2) {
+    this.plot = line1.plot;
+    this.line1 = line1;
+    this.line2 = line2;
+    line1.listeners.push(this);
+    line2.listeners.push(this);
+    this.updated(line1, line2);
 }
+
+DynPointIntersection.prototype.updated = function(line)
+{
+console.log('point intersection updated');
+    var pts = getIntersection(this.line1.path.node(), this.line2.path.node());
+console.log('\t' + pts[0][0] + ',' + pts[0][1]);
+    var c = this.plot.graph.selectAll('circle.intersection')
+        .data(pts)
+        .attr('cx', function(d) {return d[0];})
+        .attr('cy', function(d) {return d[1];});
+    c.enter()
+        .append('circle')
+        .attr('class', 'intersection')
+        .attr('cx', function(d) {return d[0];})
+        .attr('cy', function(d) {return d[1];})
+        .attr('r', 6);
+//    c.exit().remove();
+};
