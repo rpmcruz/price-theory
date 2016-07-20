8b343958c251127cd0cd3727835927ab27574b28
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-07-20 08:15:48 +0100

    - Using actual utility function - Using MathJax for equations rendering

---
+++ pkgs/econ.utility.js
@@ -0,0 +1,12 @@
+function Utility(alphas)
+{
+    this.as = alphas;
+}
+
+Utility.prototype.get = function(goods)
+{
+    var ret = 0;
+    for(var i = 0; i < goods.length; i++)
+        ret += (1-Math.exp(-this.as[i]* goods[i])) / goods.length;
+    return ret;
+};
